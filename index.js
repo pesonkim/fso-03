d17c851adb9b1b12ba3cf88531288ce0580e625d
kpesonen <pesonkim@gmail.com>
2021-09-03 16:07:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+const { response } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let data = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(data)
+})
+
+app.get('/info', (request, response) => {
+    console.log(response)
+    response.send(`<p>Phonebook has info for ${data.length} people</p><p>${new Date()}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
