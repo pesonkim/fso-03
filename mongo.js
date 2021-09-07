c185dfc4617e23841cdf93d24d71199899b34af3
kpesonen <pesonkim@gmail.com>
2021-09-07 12:55:06 +0300

    latest

--- mongo.js
+++ mongo.js
@@ -24,12 +24,12 @@ if (process.argv.length === 5) {
         name: process.argv[3],
         number: process.argv[4],
     })
-    
-    person.save().then(result => {
+
+    person.save().then(() => {
         console.log(`added ${person.name} number ${person.number} to phonebook`)
         mongoose.connection.close()
     })
-    
+
 } else {
     Person.find({}).then(result => {
         console.log('phonebook:')
